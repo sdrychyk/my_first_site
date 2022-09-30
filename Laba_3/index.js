bb107f4dac98d211288dabe19d24f7bf19be01f5
sdrychyk <slavadrychyk04@gmail.com>
2022-09-30 20:19:36 +0300

    Laba.3-1

---
+++ Laba_3/index.js
@@ -0,0 +1,65 @@
+console.log('-----------------------Task 1...--------------------------')
+console.log(' -------- ');
+console.log('|# # # # |');
+console.log('| # # # #|');
+console.log('|# # # # |');
+console.log('| # # # #|');
+console.log('|# # # # |');
+console.log('| # # # #|');
+console.log('|# # # # |');
+console.log(' -------- ');
+
+console.log('-----------------------Task 2...--------------------------')
+
+for(let i=1; i<101;i++){
+    if(i%3===0){
+        console.log("any slovo =)");
+    }
+    else{console.log(i)};
+}
+
+console.log('-----------------------Task 3...--------------------------')
+
+
+
+let str1 ="" + 1 + 0; // string type
+console.log( str1);
+let str2 ="" - 1 + 0; // number type
+console.log( str2);
+let str3 = true + false; // number type
+console.log(str3);
+let str17 = null + 1; // number type
+console.log( str17);
+let str14 = 2 && 5;
+console.log( str14); // number type
+let str4 = 6 / "3";
+console.log(  str4) // number type
+let str5="2" * "3" ;// number type
+console.log(typeof str5)
+let str6=4 + 5 + "px" ;// string type
+console.log( str6)
+let str7= "$" + 4 + 5;// string type
+console.log(  str7)
+let str8="4" - 2 ;// number type
+console.log( str8)
+let str9="4px" - 2 ;// number type
+console.log( str9)
+let str10= 7 / 0;// number type
+console.log( str10)
+let str11= "  -9\n" + 5; // string type
+console.log( str11)
+let str12="  -9\n" - 5 ;// number type
+console.log( str12)
+let str0=5 && 2 ;// number type
+console.log( str0)
+let str13= 5 || 0;// number type
+console.log( str13)
+let str15= 0 || 5;// number type
+console.log( str15)
+let str16=undefined + 1 ;// number type
+console.log( str16)
+let str18=null == "\n0\n" ;//boolean type
+console.log( str18)
+let str19= +null == +"\n0\n" //boolean type
+;
+console.log( str19)
